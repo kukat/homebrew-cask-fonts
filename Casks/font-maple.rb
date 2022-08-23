030e68c2c94064b13b6713236607f86368ac7aa0
Cheng Yao <kukathe@gmail.com>
2022-08-23 16:19:31 +0800

    Create font-maple.rb
    Add font: Maple-font (https://github.com/subframe7536/Maple-font)

---
+++ Casks/font-maple.rb
@@ -0,0 +1,12 @@
+cask "font-maple" do
+  version "5.0"
+  desc "Open source monospace/Nerd Font font with round corner for IDE and command line. 开源圆角的等宽和控制台字体"
+  homepage "https://github.com/subframe7536/Maple-font"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v5.0/Maple.Mono.ttf.zip"
+  sha256 "14aae22b953be17ba1fe21ec08c69e47158790a8f3bfa6db81c1c04e6ef4f05a"
+
+  font "MapleMono-Regular.ttf"
+  font "MapleMono-Bold.ttf"
+  font "MapleMono-BoldItalic.ttf"
+  font "MapleMono-Italic.ttf"
+end
